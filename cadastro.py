bbc1cd74a5bb10f68403fe18ffbb538803b0d0b8
Leonardogotz <leonardogotz@gmail.com>
2023-07-10 00:25:53 -0300

    Banco de dados
    Realizei a criação de um banco de dados, para armazenar as informações dos usuarios cadastrados.

--- cadastro.py
+++ cadastro.py
@@ -1,22 +1,43 @@
 from PyQt5 import  uic,QtWidgets
+import mysql.connector
+
+banco = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    database="cadastro"
+    )
+
 
 def funcao_principal():
     linha1 = interface.lineEdit.text()
     linha2 = interface.lineEdit_2.text()
     linha3 = interface.lineEdit_3.text()
+
+    perfil = ""
     
     if interface.radioButton.isChecked() :
-        print("Perfil Suprimentos selecionada")
+        print("Perfil Suprimentos selecionado")
+        perfil ="Suprimentos"
     elif interface.radioButton_2.isChecked() :
-        print("Perfil Contabilidade selecionada")
+        print("Perfil Contabilidade selecionado")
+        perfil ="Contabilidade"
     elif interface.radioButton_3.isChecked() :
-        print("Categoria Comercial selecionada")    
+        print("Perfil Comercial selecionado")
+        perfil ="Comercial"    
     else :
-        print("Categoria Alimentos selecionada")
+        print("Perfil Logistica selecionado")
+        perfil ="Logistica"
 
     print("Nome do Usuário:",linha1)
     print("CPF:",linha2)
     print("Código do Sistema:",linha3)
+
+    cursor = banco.cursor()
+    comando_SQL = "INSERT INTO cadastro (usuario, cpf, codigo, perfil) VALUES (%s,%s,%s,%s)"
+    dados = (str(linha1),str(linha2),str(linha3),perfil)
+    cursor.execute(comando_SQL,dados)
+    banco.commit()
     
 
 app=QtWidgets.QApplication([])
