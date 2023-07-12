987d95855018a1f988bab32673b9bd298b8dfa2e
Leonardogotz <leonardogotz@gmail.com>
2023-07-12 00:13:21 -0300

    Criação da restrição de matriz
    Criação do código de restrição de permissões de usuários do sistema.

--- cadastro.py
+++ cadastro.py
@@ -12,8 +12,23 @@ banco = mysql.connector.connect(
 def funcao_principal():
     linha1 = interface.lineEdit.text()
     linha2 = interface.lineEdit_2.text()
-    linha3 = interface.lineEdit_3.text()
 
+    codigo = ""
+    
+    if interface.checkBox.isChecked() :
+        print("Setor compras selecionado")
+        codigo ="Compras"
+    elif interface.checkBox_2.isChecked() :
+        print("Setor administrativo selecionado")
+        codigo ="Administrativo"
+    elif interface.checkBox_3.isChecked() :
+        print("Setor estoque selecionado")
+        codigo ="Estoque"    
+    elif interface.checkBox_4.isChecked() :
+        print("Setor vendas selecionado")
+        codigo ="Vendas"
+
+    
     perfil = ""
     
     if interface.radioButton.isChecked() :
@@ -25,27 +40,39 @@ def funcao_principal():
     elif interface.radioButton_3.isChecked() :
         print("Perfil Comercial selecionado")
         perfil ="Comercial"    
-    else :
+    elif interface.radioButton_4.isChecked() :
         print("Perfil Logistica selecionado")
         perfil ="Logistica"
 
     print("Nome do Usuário:",linha1)
     print("CPF:",linha2)
-    print("Código do Sistema:",linha3)
 
-    cursor = banco.cursor()
-    comando_SQL = "INSERT INTO cadastro (usuario, cpf, codigo, perfil) VALUES (%s,%s,%s,%s)"
-    dados = (str(linha1),str(linha2),str(linha3),perfil)
-    cursor.execute(comando_SQL,dados)
-    banco.commit()
-    interface.lineEdit.setText("")
-    interface.lineEdit_2.setText("")
-    interface.lineEdit_3.setText("")
     
-    sucesso.show()
+    if interface.checkBox.isChecked() and interface.radioButton_2.isChecked() or interface.radioButton_4.isChecked():
+        erro.show()
+    elif interface.checkBox_2.isChecked() and interface.radioButton.isChecked() or interface.radioButton_3.isChecked():
+        erro.show()
+    elif interface.checkBox_3.isChecked() and interface.radioButton_2.isChecked() or interface.radioButton_3.isChecked():
+        erro.show()
+    elif interface.checkBox_4.isChecked() and interface.radioButton_2.isChecked() or interface.radioButton_4.isChecked():
+        erro.show()          
+    else: 
+        cursor = banco.cursor()
+        comando_SQL = "INSERT INTO cadastro (usuario, cpf, codigo, perfil) VALUES (%s,%s,%s,%s)"
+        dados = (str(linha1),str(linha2),codigo,perfil)
+        cursor.execute(comando_SQL,dados)
+        banco.commit()
+        interface.lineEdit.setText("")
+        interface.lineEdit_2.setText("")
+    
+        sucesso.show()
 
 def fechar_pagina():
     sucesso.close()
+
+def fechar_erro():
+    erro.close()
+
 
 def lista_usuarios():
     lista.show()
@@ -81,10 +108,12 @@ app=QtWidgets.QApplication([])
 interface=uic.loadUi("interface.ui")
 lista=uic.loadUi("lista.ui")
 sucesso=uic.loadUi("sucesso.ui")
+erro=uic.loadUi("erro.ui")
 interface.pushButton.clicked.connect(funcao_principal)
 interface.pushButton_2.clicked.connect(lista_usuarios)
 lista.pushButton.clicked.connect(deletar_dados)
 sucesso.pushButton.clicked.connect(fechar_pagina)
+erro.pushButton.clicked.connect(fechar_erro)
 
 
 interface.show()
