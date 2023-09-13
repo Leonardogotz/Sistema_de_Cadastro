89fac0b10bcc8dff3d65ab3bfea98b6d90ce5c62
Leonardogotz <leonardogotz@gmail.com>
2023-09-13 10:33:57 -0300

    new improvements
    I added a new secondary code field and fixed errors when the user doesn't write anything in the fields

--- cadastro.py
+++ cadastro.py
@@ -15,36 +15,40 @@ def funcao_principal():
     linha2 = interface.lineEdit_2.text()
 
     codigo_1 = ""
-    codigo_2 = ""
-    codigo_3 = ""
-    codigo_4 = ""
     
     if interface.checkBox.isChecked() :
         print("Setor compras selecionado")
         codigo_1 ="Suprimentos"
-    else:
-        codigo_1 == None
 
 
-    if interface.checkBox_2.isChecked() :
+    elif interface.checkBox_2.isChecked() :
         print("Setor administrativo selecionado")
-        codigo_2 ="Contabilidade"
-    else:
-        codigo_2 == None
+        codigo_1 ="Contabilidade"
         
-    if interface.checkBox_3.isChecked() :
+    elif interface.checkBox_3.isChecked() :
         print("Setor estoque selecionado")
-        codigo_3 ="Comercial"
-    else:
-        codigo_3 == None
+        codigo_1 ="Comercial"
 
-    if interface.checkBox_4.isChecked() :
+    elif interface.checkBox_4.isChecked() :
         print("Setor vendas selecionado")
-        codigo_4 ="Logistica"
-    else:
-        codigo_4 == None
-   
-    codigos = (codigo_1,codigo_2,codigo_3,codigo_4)
+        codigo_1 ="Logistica"
+
+    
+    codigo_2= ""
+
+    if (interface.checkBox.isChecked() and interface.checkBox_2.isChecked()):
+            codigo_2 = "Contabilidade"
+    if (interface.checkBox.isChecked() and interface.checkBox_3.isChecked()):
+            codigo_2 = "Comercial"
+    if (interface.checkBox.isChecked() and interface.checkBox_4.isChecked()):
+            codigo_2 = "Logistica"
+    if (interface.checkBox_2.isChecked() and interface.checkBox_3.isChecked()):
+            codigo_2 = "Comercial"
+    if (interface.checkBox_2.isChecked() and interface.checkBox_4.isChecked()):
+            codigo_2 = "Logistica"
+    if (interface.checkBox_3.isChecked() and interface.checkBox_4.isChecked()):
+            codigo_2 = "Logistica"
+
 
     
     perfil = ""
@@ -63,30 +67,47 @@ def funcao_principal():
         perfil ="Vendas"
     
     
-    print(perfil)
-    print(codigos)
-    print("Nome do Usuário:",linha1)
-    print("CPF:",linha2)
+    if perfil == "":
+         erro2.show()
+    else:
+        print(perfil)
+    
+        if codigo_1 == "":
+            erro2.show()
+        else:
+            print(codigo_1)
+            print(codigo_2)
+        
+        
+            if linha1 == "":
+                erro2.show()
+            else:
+                print("Nome do Usuário:",linha1)
 
-    
-    if interface.radioButton.isChecked() and (interface.checkBox_2.isChecked() or interface.checkBox_4.isChecked()):
-        erro.show()
-    elif interface.radioButton_2.isChecked() and (interface.checkBox.isChecked() or interface.checkBox_3.isChecked()):
-        erro.show()
-    elif interface.radioButton_3.isChecked() and (interface.checkBox_2.isChecked() or interface.checkBox_3.isChecked()):
-        erro.show()
-    elif interface.radioButton_4.isChecked() and (interface.checkBox_2.isChecked() or interface.checkBox_4.isChecked()):
-        erro.show()          
-    else: 
-        cursor = banco.cursor()
-        comando_SQL = "INSERT INTO cadastro (usuario, cpf, codigos, perfil) VALUES (%s,%s,%s,%s)"
-        dados = (str(linha1),str(linha2), str(codigos), perfil)
-        cursor.execute(comando_SQL,dados)
-        banco.commit()
-        interface.lineEdit.setText("")
-        interface.lineEdit_2.setText("")
-    
-        sucesso.show()
+                if linha2 == "":
+                    erro2.show()
+                else:
+                    print("CPF:", linha2)
+
+            
+                    if interface.radioButton.isChecked() and (interface.checkBox_2.isChecked() or interface.checkBox_4.isChecked()):
+                        erro.show()
+                    elif interface.radioButton_2.isChecked() and (interface.checkBox.isChecked() or interface.checkBox_3.isChecked()):
+                        erro.show()
+                    elif interface.radioButton_3.isChecked() and (interface.checkBox_2.isChecked() or interface.checkBox_3.isChecked()):
+                        erro.show()
+                    elif interface.radioButton_4.isChecked() and (interface.checkBox_2.isChecked() or interface.checkBox_4.isChecked()):
+                        erro.show()          
+                    else: 
+                        cursor = banco.cursor()
+                        comando_SQL = "INSERT INTO cadastro (usuario, cpf, codigo_1, codigo_2, perfil) VALUES (%s,%s,%s,%s,%s)"
+                        dados = (str(linha1),str(linha2), codigo_1, codigo_2, perfil)
+                        cursor.execute(comando_SQL,dados)
+                        banco.commit()
+                        interface.lineEdit.setText("")
+                        interface.lineEdit_2.setText("")
+                    
+                        sucesso.show()
 
 def fechar_pagina():
     sucesso.close()
@@ -104,10 +125,10 @@ def lista_usuarios():
     dados_lidos = cursor.fetchall()
 
     lista.tableWidget.setRowCount(len(dados_lidos))
-    lista.tableWidget.setColumnCount(5)
+    lista.tableWidget.setColumnCount(6)
 
     for i in range(0, len(dados_lidos)):
-        for j in range(0, 5):
+        for j in range(0, 6):
             lista.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
 
 
@@ -130,6 +151,7 @@ interface=uic.loadUi("interface.ui")
 lista=uic.loadUi("lista.ui")
 sucesso=uic.loadUi("sucesso.ui")
 erro=uic.loadUi("erro.ui")
+erro2=uic.loadUi("erro_dados.ui")
 teladeentrada=uic.loadUi("teladeentrada.ui")
 perfil_conflitos=uic.loadUi("conflitos.ui")
 interface.pushButton.clicked.connect(funcao_principal)
@@ -139,6 +161,7 @@ lista.pushButton.clicked.connect(deletar_dados)
 lista.pushButton_2.clicked.connect(lista.close)
 sucesso.pushButton.clicked.connect(fechar_pagina)
 erro.pushButton.clicked.connect(fechar_erro)
+erro2.pushButton.clicked.connect(erro2.close)
 teladeentrada.consulta.clicked.connect(lista_usuarios)
 teladeentrada.cadastro.clicked.connect(interface.show)
 teladeentrada.sair2.clicked.connect(perfil_conflitos.show)
